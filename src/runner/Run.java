afa7ebb6a5b81702862c2044301e69000f927eba
Yeison Molano <kolokololoco79mail.com>
2023-09-25 10:44:46 -0500

    poject structur

---
+++ src/runner/Run.java
@@ -0,0 +1,10 @@
+package runner;
+
+import controller.Controller;
+
+public class Run {
+	public static void main(String[] args) {
+		Controller control = new Controller();
+		control.init();
+	}
+}
